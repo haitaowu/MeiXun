aa1979788a3e94158a3b1aef4d944390660244b7
haitaowu <taotaowoo@gmail.com>
2017-08-11 22:09:03 +0800

    first commit

---
+++ MeiXun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiXun
+//
+//  Created by taotao on 2017/8/11.
+//  Copyright © 2017年 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiXun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiXun
+//
+//  Created by taotao on 2017/8/11.
+//  Copyright © 2017年 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
