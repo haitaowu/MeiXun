e3c73dc464e287e2e79627c4241304d8a926c97f
haitaowu <taotaowoo@gmail.com>
2017-08-12 18:16:56 +0800

    验证码输入label 添加边框。

--- MeiXun/MClasses/Login/Controllers/LoginCodeTableViewController.m
+++ MeiXun/MClasses/Login/Controllers/LoginCodeTableViewController.m
@@ -9,6 +9,8 @@
 #import "LoginCodeTableViewController.h"
 
 @interface LoginCodeTableViewController ()
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *enterLabels;
+@property (weak, nonatomic) IBOutlet UITextField *enterField;
 
 @end
 
@@ -16,12 +18,45 @@
 #pragma mark - override methods
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupViewUI];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+#pragma mark - setup UI 
+- (void)setupViewUI
+{
+    for (UILabel *label in self.enterLabels) {
+        label.layer.borderWidth = 1;
+        label.layer.borderColor = [UIColor grayColor].CGColor;
+    }
+}
+
+#pragma mark - selectors
+- (IBAction)tapEnterBtn:(id)sender {
+    [self.enterField becomeFirstResponder];
+}
+
+#pragma mark - UITableView --- Table view  delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 0.001;
+}
+
+#pragma mark - UITableView --- Table view  delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
+}
+
+#pragma mark - UIScrollView delegate
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self.view endEditing:YES];
 }
 
 
